305207080c8f7cc167fd04fc1ccd6783577d4d59
ELehotzky <Lehotzky.Erica@gmail.com>
2019-01-19 21:05:34 -0600

    trying out valueUpdate

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ let object = {
 	lastName: ko.observable("Lehotzky")
 };
 
-object.fullName = ko.computed(function() {
-	return object.firstName() + " " + object.lastName();
+// object.fullName = ko.computed(function() {
+// 	return object.firstName() + " " + object.lastName();
+// });
+
+object.firstName.subscribe(function(value) {
+	console.log("First Name has been changed");
+	console.log(value);
 });
 
 ko.applyBindings(object);
