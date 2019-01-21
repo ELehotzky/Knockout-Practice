a3f109b65624147023f2b1432433ca4f708f2219
ELehotzky <Lehotzky.Erica@gmail.com>
2019-01-20 18:24:32 -0600

    displaying data from array - easy peasy

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 let object = {
 	firstName: ko.observable("Erica"),
-	lastName: ko.observable("Lehotzky")
+	lastName: ko.observable("Lehotzky"),
+	friends: ko.observableArray([new Friend("Alanna"), new Friend("Jessica")])
 };
 
 // object.fullName = ko.computed(function() {
 // 	return object.firstName() + " " + object.lastName();
 // });
 
-object.firstName.subscribe(function(value) {
-	console.log("First Name has been changed");
-	console.log(value);
-});
+// object.firstName.subscribe(function(value) {
+// 	console.log("First Name has been changed");
+// 	console.log(value);
+// });
+
+function Friend(name) {
+	this.name = name;
+}
 
 ko.applyBindings(object);
